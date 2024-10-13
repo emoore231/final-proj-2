c73f74d6c9980bf8672bc66575951415157bf84b
Infinity Owner <infiniplex@outlook.com>
2024-10-12 22:01:43 -0400

    hsbdf

--- compile.sh
+++ compile.sh
@@ -7,6 +7,7 @@ c_files=$(find . -maxdepth 1 -name "*.c")
 
 cpp_objects=""
 for file in $cpp_files; do
+    echo "Compiling $file"
     obj_file="${file%.cpp}.o"
     g++ -g -c "$file" -o "$obj_file"
     cpp_objects="$cpp_objects $obj_file"
@@ -14,10 +15,13 @@ done
 
 c_objects=""
 for file in $c_files; do
+    echo "Compiling $file"
     obj_file="${file%.c}.o"
     gcc -g -c "$file" -o "$obj_file"
     c_objects="$c_objects $obj_file"
 done
+
+echo "Linking files"
 
 g++ -g $cpp_objects $c_objects -o "$output"
 g++ -g $cpp_objects $c_objects -o main2
